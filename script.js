d0b7a93ab078d7c8e2f9363f64050f1f95108351
F-Bayram <bayferdi@outlook.com>
2022-08-31 14:45:14 +0200

    operate function created

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ function multiply(a, b){
 }
 
 function divide(a, b){
-    return a/b;
+    return b == 0 ? undefined : a/b;
+}
+
+function operate(operator, a, b){
+    var res = 0;
+
+    if (new RegExp("add", 'gi').test(operator))
+        res = add(a, b);
+    else if (new RegExp("sub", 'gi').test(operator))
+        res = sub(a, b);
+    else if (new RegExp("multiply", 'gi').test(operator))
+        res = multiply(a, b);
+    else if (new RegExp("divide", 'gi').test(operator))
+        res = divide(a, b);
+    else res = undefined;
+
+    return res;
 }
